6d114a8cfbd2c4d1ef8e2ecb2befc492368bd697
jcalbrecht <jcalbrecht85@gmail.com>
2021-08-04 20:44:02 -0500

    add repay tests

--- scripts/main.py
+++ scripts/main.py
@@ -5,26 +5,30 @@ from web3 import Web3
 # network.connect()
 
 dai_addrs =  {'address': "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063", "amAddress": "0x27f8d03b3a2196956ed754badc28d73be8830a6e", "debtAddress": "0x75c4d1Fb84429023170086f06E682DcbBF537b7d"}
+dai = dai_addrs['address']
+amDai = dai_addrs['amAddress']
+debtDai = dai_addrs['debtAddress']
 
 
-def main():
-    dai = dai_addrs['address']
-    amDai = dai_addrs['amAddress']
-    debtDai = dai_addrs['debtAddress']
-
-    user = Controller(dai_addrs)
-    user.approve_all()
-    user.get_erc20(user.account.balance() * 0.9)
-    
-    for _ in range(5):
+def stack_tokens(user: Controller):
+    for _ in range(6):
         user.deposit_erc20(user.balanceOf(dai))
         user.borrow_erc20(user.get_stable_borrow_amount())
     user.deposit_erc20(user.balanceOf(dai))
 
+def cash_out(user: Controller):
     while user.balanceOf(debtDai) > 0:
         withdraw_amount = int((user.balanceOf(amDai) - user.balanceOf(debtDai)) * 0.4)
         user.withdraw_erc20(withdraw_amount)
         user.repay_erc20(user.balanceOf(dai))
+
+def main():
+    user = Controller(dai_addrs)
+    user.approve_all()
+    user.get_erc20(user.account.balance() * 0.9)
+    
+    stack_tokens(user)
+
 
 if __name__ == '__main__':
     main()
--- tests/test_yield.py
+++ tests/test_yield.py
@@ -5,6 +5,9 @@ from brownie.network.account import Account
 from scripts.marketdata import market_data
 from scripts.utils import *
 
+dai_addr = market_data['dai']['address']
+amDai_addr = market_data['dai']['amAddress']
+debtDai_addr = market_data['dai']['debtAddress']
 
 @pytest.fixture(scope="module")
 def controller():
@@ -12,7 +15,6 @@ def controller():
 
 
 def test_eth_to_erc_swap(controller):
-    dai_addr = market_data['dai']['address']
 
     assert controller.account.balance() > 0
     assert controller.balanceOf(dai_addr) == 0
@@ -23,17 +25,13 @@ def test_eth_to_erc_swap(controller):
     assert controller.balanceOf(dai_addr) > 0
 
 def test_aave_deposit_erc(controller):
-    dai_addr = market_data['dai']['address']
-    amDai_addr = market_data['dai']['amAddress']
 
     controller.deposit_erc20(controller.balanceOf(dai_addr))
     
     assert controller.balanceOf(amDai_addr) > 0
 
 def test_aave_borrow_erc(controller):
-    dai_addr = market_data['dai']['address']
-    amDai_addr = market_data['dai']['amAddress']
-    debtDai_addr = market_data['dai']['debtAddress']
+
 
     assert controller.balanceOf(debtDai_addr) == 0
     
@@ -45,9 +43,6 @@ def test_aave_borrow_erc(controller):
     
 
 def test_aave_withdraw_erc(controller):
-    dai_addr = market_data['dai']['address']
-    amDai_addr = market_data['dai']['amAddress']
-    debtDai_addr = market_data['dai']['debtAddress']
 
     assert controller.balanceOf(amDai_addr) > 0
 
@@ -57,3 +52,14 @@ def test_aave_withdraw_erc(controller):
     controller.withdraw_erc20(withdraw_amount)
 
     assert controller.balanceOf(dai_addr) > before_balance
+
+def test_aave_repay_erc(controller):
+    balance = controller.balanceOf(dai_addr)
+    debt = controller.balanceOf(debtDai_addr)
+
+    assert balance > 0
+    assert debt > 0
+
+    controller.repay_erc20(balance)
+
+    assert controller.balanceOf(debtDai_addr) < debt
